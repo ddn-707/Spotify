920f1eb68f4d219cb330436850f7a43f3d133ccf
dinhdung140198 <dinhdung.jr7@gmail.com>
2024-04-10 23:12:14 +0700

    create project Spotify iOS

---
+++ Spotify-iOS/Spotify-iOS/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify-iOS
+//
+//  Created by DND on 10/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "HOME"
+        view.backgroundColor = .orange
+    }
+    
+}
+
